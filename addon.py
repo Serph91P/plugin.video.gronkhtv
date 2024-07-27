9267abff2bb796a967183379e7d7931596ce57f4
Seraph91P <maximilianebert1991@gmail.com>
2024-07-27 15:07:21 +0200

    feat(chapter): added custom button for chapters because kodi cannot read the chapters by itself

--- addon.py
+++ addon.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Module: main
-# Author: Schattenechse
+# Author: Seraph91P
 # Based on example by: Roman V. M.
-# Created on: 20.02.2022
+# Created on: 27.07.2024    
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import sys
@@ -41,10 +41,30 @@ _UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
 # Globale Variable für den Kapitel-Cache
 chapter_cache = {}
 
+# Custom OSD class
+class CustomOSD(xbmcgui.WindowXMLDialog):
+    def __init__(self, *args, **kwargs):
+        self.chapters = kwargs.get('chapters', [])
+        self.player = xbmc.Player()
+        super(CustomOSD, self).__init__(*args, **kwargs)
+
+    def onInit(self):
+        self.chapter_button = self.getControl(9000)
+        self.chapter_button.setLabel("Chapters")
+
+    def onClick(self, controlId):
+        if controlId == 9000:
+            chapter_offset = show_chapter_menu(self.chapters)
+            if chapter_offset is not None:
+                self.player.seekTime(chapter_offset)
+                self.close()
+
 # Funktion zum Abrufen und Cachen der Kapitel
 @lru_cache(maxsize=100)  # Speichert die letzten 100 Aufrufe
 def get_chapters(episode):
+    xbmc.log(f"[Gronkh.tv] Fetching chapters for episode {episode}", xbmc.LOGINFO)
     if episode in chapter_cache:
+        xbmc.log(f"[Gronkh.tv] Chapters found in cache for episode {episode}", xbmc.LOGINFO)
         return chapter_cache[episode]
     
     req = urlopen(f'https://api.gronkh.tv/v1/video/info?episode={episode}')
@@ -52,6 +72,7 @@ def get_chapters(episode):
     chapters = json.loads(content)["chapters"]
     
     chapter_cache[episode] = chapters
+    xbmc.log(f"[Gronkh.tv] Fetched {len(chapters)} chapters for episode {episode}", xbmc.LOGINFO)
     return chapters
 
 def get_url(**kwargs):
@@ -63,7 +84,6 @@ def get_url(**kwargs):
     :rtype: str
     """
     return '{}?{}'.format(_URL, urlencode(kwargs))
-
 
 def get_categories():
     """
@@ -82,7 +102,6 @@ def get_categories():
 
     return _CATEGORIES
 
-
 def get_playlist_url(episode):
     """
     Get Playlist-URL from episode number
@@ -93,70 +112,34 @@ def get_playlist_url(episode):
 
     return playlist_url
 
-
 def get_videos(category, offset=0, search_str=""):
-    """
-    Get the list of videofiles/streams.
-
-    Here you can insert some parsing code that retrieves
-    the list of video streams in the given category from some site or API.
-
-    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :param category: Category name
-    :type category: str
-    :param offset: offset for browsing/searching
-    :type offset: int
-    :param search_str: Search string
-    :type search_str: str
-    :return: the list of videos in the category
-    :rtype: list
-
-    structure of entries:
-        id: int
-        title: str
-        created_at: str (YYYY-MM-DD HH:mm:ss)
-        epsiode: int
-        preview_url: str
-        video_length: int
-        views: int
-        tags: [{id: int, title: str}]
-    """
     videos = []
-    QUERY = ""
 
     if category == _CATEGORIES[0]:
         req = urlopen("https://api.gronkh.tv/v1/video/discovery/recent")
         content = req.read().decode("utf-8")
-        entries = json.loads(content)["discovery"]
-        videos = entries
+        videos = json.loads(content)["discovery"]
     elif category == _CATEGORIES[1]:
         req = urlopen("https://api.gronkh.tv/v1/video/discovery/views")
         content = req.read().decode("utf-8")
-        entries = json.loads(content)["discovery"]
-        videos = entries
+        videos = json.loads(content)["discovery"]
     elif category == _CATEGORIES[2]:
         OFFSET = offset
-        NUM = 25 #25 is max
+        NUM = 25
         req = urlopen(f'https://api.gronkh.tv/v1/search?sort=date&offset={OFFSET}&first={NUM}')
         content = req.read().decode("utf-8")
-        entries = json.loads(content)["results"]["videos"]
-        videos = entries
+        videos = json.loads(content)["results"]["videos"]
     elif category == _CATEGORIES[3]:
-        QUERY = search_str if search_str != "" else xbmcgui.Dialog().input("Suche", type=xbmcgui.INPUT_ALPHANUM)
-        while len(QUERY) < 3: # Suchbegriff muss mind. 3 Zeichen lang sein.
-            if QUERY == "": # Wenn nichts gesucht wird, wird auch nichts gefunden.
+        search_query = search_str if search_str != "" else xbmcgui.Dialog().input("Suche", type=xbmcgui.INPUT_ALPHANUM)
+        while len(search_query) < 3:
+            if search_query == "":
                 return videos, ""
-            # Frage erneut nach Suchbegriff, bis mind. 3 Zeichen eingegeben wurden oder abgebrochen wurde (leerer String)
             xbmcgui.Dialog().ok(_plugin, _addon.getLocalizedString(30101))
-            QUERY = search_str if search_str != "" else xbmcgui.Dialog().input("Suche", type=xbmcgui.INPUT_ALPHANUM)
-        req = urlopen(f'https://api.gronkh.tv/v1/search?query={quote_plus(QUERY)}')
+            search_query = search_str if search_str != "" else xbmcgui.Dialog().input("Suche", type=xbmcgui.INPUT_ALPHANUM)
+        req = urlopen(f'https://api.gronkh.tv/v1/search?query={quote_plus(search_query)}')
         content = req.read().decode("utf-8")
-        entries = json.loads(content)["results"]["videos"]
-        videos = entries
-    return videos, QUERY
-
+        videos = json.loads(content)["results"]["videos"]
+    return videos, search_query if category == _CATEGORIES[3] else ""
 
 def list_categories():
     """
@@ -174,18 +157,6 @@ def list_categories():
     for category in categories:
         # Create a list item with a text label and a thumbnail image.
         list_item = xbmcgui.ListItem(label=category)
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-##        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-##                          'icon': VIDEOS[category][0]['thumb'],
-##                          'fanart': VIDEOS[category][0]['thumb']})
-        # Set additional info for the list item.
-        # Here we use a category name for both properties for for simplicity's sake.
-        # setInfo allows to set various information for an item.
-        # For available properties see the following link:
-        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
-        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
         list_item.setInfo('video', {'title': category,
                                     'genre': 'Streams und Let\'s Plays',
                                     'mediatype': 'video'})
@@ -201,58 +172,15 @@ def list_categories():
     # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_HANDLE)
 
-
 def list_videos(category, offset=0, search_str=""):
-    """
-    Create the list of playable videos in the Kodi interface.
-
-    :param category: Category name
-    :type category: str
-    :param offset: offset for browsing/searching
-    :type offset: int
-    :param search_str: Search string
-    :type search_str: str
-
-    structure of entries:
-        id: int
-        title: str
-        created_at: str (YYYY-MM-DD HH:mm:ss)
-        epsiode: int
-        preview_url: str
-        video_length: int
-        views: int
-        tags: [{id: int, title: str}]
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
     xbmcplugin.setPluginCategory(_HANDLE, category)
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
     xbmcplugin.setContent(_HANDLE, 'videos')
-    # Get the list of videos in the category.
     videos, query = get_videos(category, offset, search_str)
-    # Iterate through videos.
+
     for video in videos:
-        # Create a list item with a text label and a thumbnail image.
         list_item = xbmcgui.ListItem(label=video['title'])
         ep = video['episode']
 
-        # Kapitel abrufen
-        chapters = get_chapters(ep)
-        
-        cm = []
-        chapters_content = []
-        for c in chapters:
-            title = str(c.get("title"))
-            offset = int(c.get("offset"))
-            percentage = float(offset) / float(video['video_length']) * 100.0
-            cm.append((f'jump to [{seconds_to_time(offset)}]: {title}', f'PlayerControl(SeekPercentage({percentage}))'))
-            chapters_content.append(f'[{seconds_to_time(offset)}]: {title}')
-        list_item.addContextMenuItems(cm)
-        plot = '\n'.join(chapters_content)
-
-        # Set additional info for the list item.
-        # 'mediatype' is needed for skin to display info for this ListItem correctly.
         tag = list_item.getVideoInfoTag()
         tag.setMediaType('video')
         tag.setTitle(video['title'])
@@ -262,121 +190,111 @@ def list_videos(category, offset=0, search_str=""):
         tag.setDateAdded(video['created_at'])
         tag.setPremiered(video['created_at'])
         tag.setFirstAired(video['created_at'])
-        tag.setPlot(plot)
 
-        xbmc.log(video['created_at'], xbmc.LOGINFO)
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
         list_item.setArt({'thumb': video['preview_url'], 'icon': video['preview_url'], 'fanart': video['preview_url']})
-        # Set 'IsPlayable' property to 'true'.
-        # This is mandatory for playable items!
         list_item.setProperty('IsPlayable', 'true')
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
         url = get_url(action='play', video=video['episode'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = False
+        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, False)
 
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    if category == _CATEGORIES[2] and len(videos) == 25 and videos[24]['episode'] != 1:
-        list_item = xbmcgui.ListItem(label=category)
-        list_item.setInfo('video', {'title': "... mehr",
-                                    'genre': 'Streams und Let\'s Plays',
-                                    'mediatype': 'video'})
-        url = get_url(action='listing', category=category, offset=int(offset)+25)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    if category == _CATEGORIES[3]:
-        if len(videos) == 0:
-            xbmc.log(f'[gronkh.tv] Kein Titel bei der Suche nach "{query}" gefunden', xbmc.LOGINFO)
-            list_item = xbmcgui.ListItem(label=f'Kein Titel unter "{query}" gefunden')
-            list_item.setInfo('video', {'title': f'Kein Titel bei der Suche nach "{query}" gefunden',
-                                        'genre': 'Streams und Let\'s Plays',
-                                        'mediatype': 'video'})
-            url = get_url(action='listing', category=category)
-            # is_folder = True means that this item opens a sub-list of lower level items.
-            is_folder = True
-            # Add our item to the Kodi virtual folder listing.
-            xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-        else:
-            if len(videos) == 25 and False: # aktuell ist nicht mit einem Limit zu rechnen
-                list_item = xbmcgui.ListItem(label=category)
-                list_item.setInfo('video', {'title': '... mehr',
-                                            'genre': 'Streams und Let\'s Plays',
-                                            'mediatype': 'video'})
-                url = get_url(action='listing', category=category, offset=int(offset)+25, search_str=query)
-                # is_folder = True means that this item opens a sub-list of lower level items.
-                is_folder = True
-                # Add our item to the Kodi virtual folder listing.
-                xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-            xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_DATEADDED)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    if category == _CATEGORIES[2] and len(videos) == 25 and videos[-1]['episode'] != 1:
+        add_more_item(category, offset)
+    elif category == _CATEGORIES[3]:
+        handle_search_results(videos, query)
+
     xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_NONE)
-    # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_HANDLE)
 
+def add_more_item(category, offset):
+    list_item = xbmcgui.ListItem(label="... mehr")
+    list_item.setInfo('video', {'title': "... mehr", 'genre': 'Streams und Let\'s Plays', 'mediatype': 'video'})
+    url = get_url(action='listing', category=category, offset=int(offset)+25)
+    xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, True)
+
+def handle_search_results(videos, query):
+    if not videos:
+        xbmc.log(f'[gronkh.tv] Kein Titel bei der Suche nach "{query}" gefunden', xbmc.LOGINFO)
+        list_item = xbmcgui.ListItem(label=f'Kein Titel unter "{query}" gefunden')
+        list_item.setInfo('video', {'title': f'Kein Titel bei der Suche nach "{query}" gefunden',
+                                    'genre': 'Streams und Let\'s Plays',
+                                    'mediatype': 'video'})
+        url = get_url(action='listing', category=_CATEGORIES[3])
+        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, True)
+    else:
+        xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_DATEADDED)
+
+def show_chapter_menu(chapters):
+    chapter_names = [f"{c['title']} ({seconds_to_time(c['offset'])})" for c in chapters]
+    dialog = xbmcgui.Dialog()
+    index = dialog.select("Select Chapter", chapter_names)
+    if index >= 0:
+        return chapters[index]['offset']
+    return None
 
 def play_video(path, episode):
+    xbmc.log(f"[Gronkh.tv] Playing video: {path}, episode: {episode}", xbmc.LOGINFO)
     play_item = xbmcgui.ListItem(path=path)
     
-    # Kapitel aus dem Cache abrufen
-    chapters = get_chapters(episode)
+    try:
+        chapters = get_chapters(episode)
+        xbmc.log(f"[Gronkh.tv] Fetched {len(chapters)} chapters for episode {episode}", xbmc.LOGINFO)
+        
+        video_tag = play_item.getVideoInfoTag()
+        video_tag.setTitle(f'Episode {episode}')
+        video_tag.setEpisode(int(episode))
+        video_tag.setMediaType('video')
+        
+        total_duration = chapters[-1]['offset'] if chapters else 0
+        video_tag.setDuration(total_duration)
+        
+        xbmcplugin.setResolvedUrl(_HANDLE, True, listitem=play_item)
+        
+        player = xbmc.Player()
+        while not player.isPlayingVideo():
+            xbmc.sleep(100)
+        
+        custom_osd = CustomOSD('CustomOSD.xml', _addon.getAddonInfo('path'), 'default', '1080i', chapters=chapters)
+        custom_osd.doModal()
+        
+    except Exception as e:
+        xbmc.log(f"[Gronkh.tv] Error processing chapters: {str(e)}", xbmc.LOGERROR)
     
-    # Kapitel im Kodi-Format hinzufügen
-    kodi_chapters = []
-    for chapter in chapters:
-        kodi_chapters.append((chapter['offset'], chapter['title']))
-    
-    play_item.setInfo('video', {'chapters': kodi_chapters})
-    
-    xbmcplugin.setResolvedUrl(_HANDLE, True, listitem=play_item)
-
-
-def router(paramstring):
-    """
-    Router function that calls other functions
-    depending on the provided paramstring
-
-    :param paramstring: URL encoded plugin paramstring
-    :type paramstring: str
-    """
-    # Parse a URL-encoded paramstring to the dictionary of
-    # {<parameter>: <value>} elements
-    params = dict(parse_qsl(paramstring))
-    # Check the parameters passed to the plugin
-    if params:
-        if params['action'] == 'listing':
-            # Display the list of videos in a provided category.
-            if params.get('offset'):
-                if params.get('search_str'):
-                    list_videos(params['category'], params['offset'], params['search_str'])
-                else:
-                    list_videos(params['category'], params['offset'])
-            else:
-                list_videos(params['category'])
-        elif params['action'] == 'play':
-            # Play a video from a provided URL.
-            play_video(get_playlist_url(params['video']), params['video'])
-        else:
-            # If the provided paramstring does not contain a supported action
-            # we raise an exception. This helps to catch coding errors,
-            # e.g. typos in action names.
-            raise ValueError('Invalid paramstring: {}!'.format(paramstring))
-    else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
-        list_categories()
+    xbmc.log("[Gronkh.tv] Video playback ended", xbmc.LOGINFO)
 
 def seconds_to_time(s):
     h = int(s / 60 / 60)
     m = int((s / 60) % 60)
     s = int(s % 60)
     return f'{h}:{m:02d}:{s:02d}'
+
+def router(paramstring):
+    xbmc.log(f"[Gronkh.tv] Router called with params: {paramstring}", xbmc.LOGINFO)
+    params = dict(parse_qsl(paramstring))
+
+    action_handlers = {
+        'listing': handle_listing,
+        'play': handle_play
+    }
+
+    if params:
+        action = params.get('action')
+        if action in action_handlers:
+            action_handlers[action](params)
+        else:
+            raise ValueError(f'Invalid action: {action}')
+    else:
+        list_categories()
+
+    xbmc.log("[Gronkh.tv] Router finished", xbmc.LOGINFO)
+
+def handle_listing(params):
+    category = params['category']
+    offset = params.get('offset', '0')
+    search_str = params.get('search_str', '')
+    list_videos(category, offset, search_str)
+
+def handle_play(params):
+    video = params['video']
+    play_video(get_playlist_url(video), video)
 
 if __name__ == "__main__":
     #set up headers for https requests
